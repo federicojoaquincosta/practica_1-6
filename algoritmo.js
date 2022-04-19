f8074357681de2ae92f0ecba5d5a021ecdcb12f2
Federico Costa <federicojoaquincosta@gmail.com>
2022-04-19 10:58:15 -0300

    tipos de datos y prompt

---
+++ algoritmo.js
@@ -0,0 +1,15 @@
+var respuesta = prompt("Ingrese su nombre", ["nombre"]);
+
+console.log(respuesta);
+
+document.write(`
+    <h4>Nombre:</h4>
+    <h4>${ respuesta }</h4>
+`)
+
+var string = "palabra";
+var numero = 2;
+var decimal = 3.4;
+var booleano = true;
+
+console.log(decimal);
